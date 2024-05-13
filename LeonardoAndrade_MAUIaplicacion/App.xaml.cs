2c01c4570ec9c18e37f767f95ea6817e8c6e1665
laboratorio <laboratorio@UPEAULA-31711>
2024-05-13 10:47:08 -0500

    Agregar archivos de proyecto.

---
+++ LeonardoAndrade_MAUIaplicacion/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LeonardoAndrade_MAUIaplicacion
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ LeonardoAndrade_MAUIaplicacion/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LeonardoAndrade_MAUIaplicacion
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
